424f94d23ba42e9252f5274da0ddfb54f350c2c0
Márton Elek <elek@apache.org>
2018-09-02 10:30:22 +0200

    fix config and verify

--- ozone101/assets/check-scm.sh
+++ ozone101/assets/check-scm.sh
@@ -1,2 +1,1 @@
-#!/usr/bin/env bash
 (( $(docker-compose  logs scm | grep Registered 2>/dev/null | wc -l) >= 1 )) && echo \"done\"
--- ozone101/step3-verify.sh
+++ ozone101/step3-verify.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-(( $(docker-compose  logs scm | grep Registered 2>/dev/null | wc -l) >= 1 )) && echo \"done\"
+./check-scm.sh
